08a15a827618aeff57a842c3301a174ef6047777
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2020-11-28 16:42:39 +0900

    Consume timeslice

--- native/steiner_tree/src/lib.rs
+++ native/steiner_tree/src/lib.rs
@@ -6,6 +6,7 @@ mod subsets;
 use rustler::codegen_runtime::{NifReturned, NIF_ENV, NIF_TERM};
 use rustler::{Encoder, Env, Error, SchedulerFlags, Term};
 use std::ffi::CString;
+use std::time::Instant;
 
 use crate::core::{compute, Ret};
 use crate::state::{
@@ -87,6 +88,7 @@ fn steiner_tree_nonyielding<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<
 /// - ptr should be the pointer created by create_state
 /// - ptr must point to the valid State
 unsafe fn steiner_tree_yielding(env: Env<'_>, ptr: *mut State) -> Term<'_> {
+    let start = Instant::now();
     let result;
     {
         let state = &mut *ptr;
@@ -105,16 +107,25 @@ unsafe fn steiner_tree_yielding(env: Env<'_>, ptr: *mut State) -> Term<'_> {
             (atoms::error(), e).encode(env)
         }
         Ret::Yielding => {
-            let encoded = encode_state_ptr_as_NIF_TERM(ptr);
-            let result = NifReturned::Reschedule {
-                fun_name: CString::new("steiner_tree_interrupted").unwrap(),
-                flags: SchedulerFlags::Normal,
-                fun: steiner_tree_interrupted,
-                args: vec![encoded],
-            };
-            // NOTE: result.apply(env).encode(env) won't work here:
-            // NIF_TERM is just an alias of usize, so it would return the Erlang representation of an integer 0.
-            Term::new(env, result.apply(env))
+            let elapsed = start.elapsed();
+            // We are given 1ms timeslice. This value is hardcoded.
+            let elapsed_micro = elapsed.as_micros().min(1000000000) as i32;
+            let percentage = (elapsed_micro / 10).max(1).min(100);
+            let should_yield = rustler::schedule::consume_timeslice(env, percentage);
+            if should_yield {
+                let encoded = encode_state_ptr_as_NIF_TERM(ptr);
+                let result = NifReturned::Reschedule {
+                    fun_name: CString::new("steiner_tree_interrupted").unwrap(),
+                    flags: SchedulerFlags::Normal,
+                    fun: steiner_tree_interrupted,
+                    args: vec![encoded],
+                };
+                // NOTE: result.apply(env).encode(env) won't work here:
+                // NIF_TERM is just an alias of usize, so it would return the Erlang representation of an integer 0.
+                Term::new(env, result.apply(env))
+            } else {
+                steiner_tree_yielding(env, ptr)
+            }
         }
     }
 }
